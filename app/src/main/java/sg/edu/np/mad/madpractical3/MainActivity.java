65f389edb18edf0cbf62a9ec902819e0c149ee2b
CliveGiam <s10257799@connect.np.edu.sg>
2024-04-29 15:15:26 +0800

    Completed practical 3

--- app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -1,6 +1,14 @@
 package sg.edu.np.mad.madpractical3;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +28,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        User user = new User("John Doe","MAD Developer",1,false);
+        TextView tvName =  findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+
+        Button btnFollow  = findViewById(R.id.btnFollow);
+
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user.followed = !(user.followed);
+
+                if(user.followed) {
+                    btnFollow.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    btnFollow.setText("Follow");
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        Intent receive = getIntent();
+        String message = receive.getStringExtra("Int");
+        tvName.setText(user.name + " " + message);
     }
+
 }
